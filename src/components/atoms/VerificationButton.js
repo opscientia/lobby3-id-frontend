4126b033160a914d881d7b91bf3b9c43d7d8909e
calebtuttle <1calebtuttle@gmail.com>
2022-08-17 17:37:15 -0400

    Add copy to initial verification page. Modify styles slightly

--- src/components/atoms/VerificationButton.js
+++ src/components/atoms/VerificationButton.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import { useAccount, useSignMessage } from 'wagmi'
+import { useState, useEffect } from "react";
+import { useAccount, useSignMessage } from "wagmi";
 
 const VerificationButton = (props) => {
   const { address } = useAccount();
@@ -7,42 +7,61 @@ const VerificationButton = (props) => {
     onSuccess(data, variables) {
       window.location.href = `http://localhost:3000/register?address=${address}&signature=${data}`;
     },
-  })
-  const [error, setError] = useState(undefined)
+  });
+  const [error, setError] = useState(undefined);
   // Get secret message to sign from server
   async function getSecretMessage() {
     try {
-      const resp = await fetch(
-        `http://localhost:3000/initialize?address=${address}`
-      )
-      return (await resp.json()).message
+      const resp = await fetch(`http://localhost:3000/initialize?address=${address}`);
+      return (await resp.json()).message;
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
 
   async function handleClick() {
-    const msg = await getSecretMessage()
+    const msg = await getSecretMessage();
     if (msg) {
-      localStorage.setItem('holoTempSecret', msg)
-      signMessage({message: msg})
-    }
-    else {
-      setError('Could not retrieve message to sign.')
+      localStorage.setItem("holoTempSecret", msg);
+      signMessage({ message: msg });
+    } else {
+      setError("Could not retrieve message to sign.");
     }
   }
 
   return (
     <>
-      {/* <div onClick={handleClick} className="verification-button x-button secondary"> */}
-      <div onClick={handleClick} className="verification-button">
-        Verify yourself
+      <div className="x-container w-container">
+        <div className="x-wrapper small-center" style={{ width: "100vw" }}>
+          <h1>What's this?</h1>
+          <p>
+            You will scan your ID document and receive information from it. We will not
+            store your personal info. You can use the data to generate a zero-knowledge
+            proof, a cryptographic proof that hides your data from everyone except for
+            you. Before clicking the button, please make sure you have the Holonym
+            extension installed.
+          </p>
+          <p>
+            Note: Only proceed if you are using a personal computer (not a public one).
+            Your info will be encrypted, but to be safe, only use a computer that is
+            solely yours or that will be used only by people you trust.
+          </p>
+          <div
+            style={{
+              maxWidth: "400px",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: "25px",
+            }}
+          >
+            <div onClick={handleClick} className="verification-button">
+              Verify yourself
+            </div>
+          </div>
+          {error && <p>Error: {error}</p>}
+        </div>
       </div>
-      {error && (
-        <p>
-          Error: {error}
-        </p>
-      )}
     </>
   );
 };
